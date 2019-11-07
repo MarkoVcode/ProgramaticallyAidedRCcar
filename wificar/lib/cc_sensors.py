63d07d6e8ef6cbd2ba456a2591a072fddbfd830d
MarkoV <marek.kaszewski@gmail.com>
2019-11-07 21:05:32 +0000

    adc exceptions

--- wificar/lib/cc_sensors.py
+++ wificar/lib/cc_sensors.py
@@ -156,13 +156,8 @@ class cc_sensors:
 
     def readSensorsI2C(self):
         if cc_configuration.isHardwareSupported():
-            print(self.sensors)
             accelerometer_data = self.accelerometer.getData()
-            print("accelerometer_data")
-            print(accelerometer_data)
             power_data = self.power.getPowerData()
-            print("power_data")
-            print(power_data)
             self.sensors['accel'] = accelerometer_data
             self.sensors['power'] = power_data
             self.sensors['accel_flt'] = self.filterAccelerometerReading(accelerometer_data)
