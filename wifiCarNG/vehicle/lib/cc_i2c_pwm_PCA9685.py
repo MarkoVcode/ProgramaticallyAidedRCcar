a1550fec18d822a097ff59e90f058699cfe67d19
MarkoV <marek.kaszewski@gmail.com>
2025-03-14 21:19:51 +0000

    updating files structure

---
+++ wifiCarNG/vehicle/lib/cc_i2c_pwm_PCA9685.py
@@ -0,0 +1,39 @@
+import time
+import logging
+import Adafruit_PCA9685
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='(%(threadName)-9s) %(message)s',)
+
+class cc_i2c_pwm_PCA9685:
+    def __init__(self):
+        self.pwm = None
+        self.init()
+ 
+    def init(self):
+        success = False
+        try:
+            self.pwm = Adafruit_PCA9685.PCA9685()
+            self.pwm.set_pwm_freq(60)
+            success = True
+        except Exception as e:
+            logging.debug('PCA9685 - not present: {0}'.format(e))
+        return success
+
+    def set_pwm(self, channel, something, pwmvalue):
+        if self.pwm is None:
+            if self.init():
+                self._set_pwm(channel, something, pwmvalue)
+        else:
+            self._set_pwm(channel, something, pwmvalue)
+
+    def _set_pwm(self, channel, something, pwmvalue):
+        try:
+            self.pwm.set_pwm(channel, something, pwmvalue)
+        except Exception as e:
+            logging.debug('PCA9685 - not present: {0}'.format(e))
+            self.pwm = None
+
+if __name__ == '__main__':
+    pwm = cc_i2c_pwm_PCA9685()
+    pwm.set_pwm(5,0, 300)
