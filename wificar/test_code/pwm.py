e92b7fb583c1370958830f3d12603152f16b34d2
MarkoV <marek.kaszewski@gmail.com>
2025-03-26 17:55:10 +0000

    pem test

--- wificar/test_code/pwm.py
+++ wificar/test_code/pwm.py
@@ -1,9 +1,9 @@
 import Adafruit_PCA9685
 
-pwm = Adafruit_PCA9685.PCA9685(address=0x41, busnum=3)
+pwm = Adafruit_PCA9685.PCA9685(address=0x40, busnum=3)
 
 pwm.set_pwm_freq(60)
-pwm.set_pwm(3, 0, x_change)
-pwm.set_pwm(2, 0, y_change)
+pwm.set_pwm(3, 0, 700)
+pwm.set_pwm(2, 0, 100)
 # Alternatively specify a different address and/or bus:
 #pwm = Adafruit_PCA9685.PCA9685(address=0x41, busnum=2)
